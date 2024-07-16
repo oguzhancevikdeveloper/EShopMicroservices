3c6e346f3db6e47a2b974b7e8e52b9b8745eb446
Oğuzhan <oguzhancevik.developer@gmail.com>
2024-07-16 16:46:41 +0300

    consumning between order and discount added

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Basket.API.Basket.StoreBasket;
+﻿using Discount.Grpc;
+
+namespace Basket.API.Basket.StoreBasket;
 public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
 public record StoreBasketResult(string UserName);
 
@@ -12,25 +14,24 @@ public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
 }
 
 public class StoreBasketCommandHandler
-    (IBasketRepository repository)
+    (IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProtoServiceClient)
     : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
-        //await DeductDiscount(command.Cart, cancellationToken);
+        await DeductDiscount(command.Cart, cancellationToken);
 
         await repository.StoreBasket(command.Cart, cancellationToken);
 
         return new StoreBasketResult(command.Cart.UserName);
     }
 
-    //private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
-    //{
-    //    // Communicate with Discount.Grpc and calculate lastest prices of products into sc
-    //    foreach (var item in cart.Items)
-    //    {
-    //        var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
-    //        item.Price -= coupon.Amount;
-    //    }
-    //}
+    private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        foreach (var item in cart.Items)
+        {
+            var coupon = await discountProtoServiceClient.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken:cancellationToken);
+            item.Price -= coupon.Amount;
+        }
+    }
 }
