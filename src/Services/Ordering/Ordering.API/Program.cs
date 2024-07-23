91e8448bade17f5da8d91c8d1761fa781e87daf6
Oğuzhan <oguzhancevik.developer@gmail.com>
2024-07-23 10:21:58 +0300

    order ınfrastructure added

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,7 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,9 +16,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseApiServices();
 
-//if (app.Environment.IsDevelopment())
-//{
-//    await app.InitialiseDatabaseAsync();
-//}
+if (app.Environment.IsDevelopment())
+{
+    await app.InitializeDatabaseAsync();
+}
 
 app.Run();
